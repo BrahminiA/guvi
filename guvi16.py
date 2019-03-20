ec1b1ec6b08bbf8755945125def825c465116e69
BrahminiA <43632503+BrahminiA@users.noreply.github.com>
2019-03-20 22:40:16 +0530

    Add files via upload

---
+++ guvi16.py
@@ -0,0 +1,10 @@
+lower,upper=input().split(" ")
+lower=int(lower)
+upper=int(upper)
+for num in range(lower,upper):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
